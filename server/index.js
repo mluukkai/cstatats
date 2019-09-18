9613d8c55a63f0bdf43ba764c08cd5ab9acbf13d
Jami Kousa <jami.kousa@helsinki.fi>
2019-09-18 16:41:50 +0300

    use our first npm package

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const cors = require('cors')
 const routes = require('@util/routes')
-const shibbolethCharsetMiddleware = require('@middleware/shibbolethCharsetMiddleware')
+const shibbolethCharsetMiddleware = require('unfuck-utf8-headers-middleware')
 const registerUserMiddleware = require('@middleware/registerUserMiddleware')
 const currentUserMiddleware = require('@middleware/currentUserMiddleware')
 const checkMiddleware = require('@middleware/checkMiddleware')
@@ -9,10 +9,18 @@ const errorMiddleware = require('@middleware/errorMiddleware')
 
 const app = express()
 
+const shibbolethHeaders = [
+  'uid',
+  'givenname', // First name
+  'mail', // Email
+  'schacpersonaluniquecode', // Contains student number
+  'sn', // Last name
+]
+
 app.use(cors())
 app.use(express.json())
 app.use(registerUserMiddleware)
-app.use(shibbolethCharsetMiddleware)
+app.use(shibbolethCharsetMiddleware(shibbolethHeaders))
 app.use(currentUserMiddleware)
 app.use(checkMiddleware)
 
