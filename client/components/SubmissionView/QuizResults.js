e04a183fa264434582c5a94e01e4b721b3627f94
Jami Kousa <jami.kousa@helsinki.fi>
2019-12-12 15:00:20 +0200

    total score

--- client/components/SubmissionView/QuizResults.js
+++ client/components/SubmissionView/QuizResults.js
@@ -7,16 +7,27 @@ const QuizResults = () => {
   const answersInCourse = user.quizAnswers[course.info.name] || {}
   const parts = Object.keys(answersInCourse || {})
   if (!parts.length) return null
-  // const wrongAnswers = user.quizAnswers.filter(a => a.right === false)
+
+  const total = parts.reduce((acc, cur) => {
+    const { score } = answersInCourse[cur]
+    if (!score) return { wrong: 0, score: 0 }
+
+    const wrong = score.total - score.right
+    return {
+      wrong: acc.wrong + wrong,
+      score: acc.score + score.points,
+    }
+  }, { wrong: 0, score: 0 })
+
   return (
     <>
-      <h3> Quiz results</h3>
+      <h3>Quiz results</h3>
       <Table celled striped>
         <Table.Header>
           <Table.Row>
-            <Table.HeaderCell> Part </Table.HeaderCell>
-            <Table.HeaderCell> Right answers </Table.HeaderCell>
-            <Table.HeaderCell> Score</Table.HeaderCell>
+            <Table.HeaderCell>Part</Table.HeaderCell>
+            <Table.HeaderCell>Right answers</Table.HeaderCell>
+            <Table.HeaderCell>Score</Table.HeaderCell>
           </Table.Row>
         </Table.Header>
         <Table.Body>
@@ -27,11 +38,16 @@ const QuizResults = () => {
             return (
               <Table.Row key={partName}>
                 <Table.Cell>{partName}</Table.Cell>
-                <Table.Cell>{score.right} / {score.total}</Table.Cell>
+                <Table.Cell>{`${score.right} / ${score.total}`}</Table.Cell>
                 <Table.Cell>{score.points}</Table.Cell>
               </Table.Row>
             )
           })}
+          <Table.Row>
+            <Table.Cell>Total</Table.Cell>
+            <Table.Cell>{`Missed: ${total.wrong}`}</Table.Cell>
+            <Table.Cell>{`${total.score}`}</Table.Cell>
+          </Table.Row>
         </Table.Body>
       </Table>
     </>
