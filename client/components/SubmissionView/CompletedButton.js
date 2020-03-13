8d846985fcab6a61bddfc3f55f2b2b28299330fa
Jami Kousa <jami.kousa@helsinki.fi>
2020-03-13 21:15:55 +0200

    triple check sure to complete

--- client/components/SubmissionView/CompletedButton.js
+++ client/components/SubmissionView/CompletedButton.js
@@ -2,16 +2,18 @@ import React, { useState } from 'react'
 import { Button } from 'semantic-ui-react'
 import { useDispatch, useSelector } from 'react-redux'
 import { getUserAction, setCourseCompletedAction, setCourseNotCompletedAction } from 'Utilities/redux/userReducer'
+import { submissionsToFullstackGradeAndCredits } from 'Utilities/common'
 
 const CompletedButton = () => {
   const dispatch = useDispatch()
   const [sure, setSure] = useState(false)
   const [disabled, setDisabled] = useState(false)
-  const { completed, courseName } = useSelector(({ course, user }) => {
+  const { completed, courseName, grade, credits } = useSelector(({ course, user }) => {
     const courseName = (course.info || {}).name
     const { completed } = (((user || {}).courseProgress || []).find(c => c.courseName === courseName) || {})
-
-    return { completed, courseName }
+    const submissions = user.submissions.filter(sub => sub.courseName === courseName)
+    const [grade, credits] = submissionsToFullstackGradeAndCredits(submissions)
+    return { completed, courseName, grade, credits }
   })
   const handleToggleCompleted = async () => {
     setTimeout(() => setDisabled(false), 1000)
@@ -20,8 +22,14 @@ const CompletedButton = () => {
     setSure(false)
 
     if (completed) await dispatch(setCourseNotCompletedAction(courseName))
-    if (!completed) await dispatch(setCourseCompletedAction(courseName))
+    if (!completed) {
+      const ok = window.confirm(`Confirm this only if you have done the exam in Moodle or in an earlier course.
 
+If you complete course now you will get ${credits} credits, grade ${grade}. Are you sure?`)
+      if (ok) {
+        await dispatch(setCourseCompletedAction(courseName))
+      }
+    }
     dispatch(getUserAction())
   }
 
