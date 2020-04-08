05e2569bb75b46b9f085a9f8b45ec1d2ee618c89
Jami Kousa <jami.kousa@helsinki.fi>
2020-04-08 15:25:23 +0300

    fix case

--- client/components/CourseAdminSuotarView/index.js
+++ client/components/CourseAdminSuotarView/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
-import DockerSuotarView from 'Components/CourseAdminSuotarView/docker/SuotarView'
-import FullstackSuotarView from 'Components/CourseAdminSuotarView/fullstack/SuotarView'
+import DockerSuotarView from 'Components/CourseAdminSuotarView/Docker/SuotarView'
+import FullstackSuotarView from 'Components/CourseAdminSuotarView/Fullstack/SuotarView'
 
 const CourseAdminSuotarView = () => {
   const { courseName } = useSelector(({ course }) => ({ courseName: course.info.name }))
