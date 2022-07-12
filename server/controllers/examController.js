9800ef9cd16de535b5318f4563e82f837a4cc86e
Matti Luukkainen <mluukkai@iki.fi>
2022-07-12 12:51:50 +0300

    2/3 required to pass

--- server/controllers/examController.js
+++ server/controllers/examController.js
@@ -160,7 +160,7 @@ const endExam = async (req, res) => {
 
   exam.points = getScore(exam.answers, questions)
 
-  exam.passed = exam.points >= questions.length / 2
+  exam.passed = 2 * exam.points >= 3 * (questions.length / 2)
 
   await exam.save()
 
