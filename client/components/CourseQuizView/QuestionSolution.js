c9e2918e2628e6d4dbc91ef04d67ed0c2b5f8dd6
Matti Luukkainen <mluukkai@iki.fi>
2022-10-27 17:44:20 +0300

    oikein vs värin

--- client/components/CourseQuizView/QuestionSolution.js
+++ client/components/CourseQuizView/QuestionSolution.js
@@ -5,25 +5,38 @@ import { Form, Segment } from 'semantic-ui-react'
 const QuestionSolution = ({ question, previousAnswers }) => {
   return (
     <Segment>
-      <h3>
-        {question.title}
-      </h3>
+      <h3>{question.title}</h3>
       <p>{question.desc}</p>
       <Form>
         {question.options.map((option) => {
-          const answered = multipleChoiceOptionChosen(previousAnswers, option.text)
+          const answered = multipleChoiceOptionChosen(
+            previousAnswers,
+            option.text,
+          )
 
-          const checkedOption = (answered && (answered.chosenValue === undefined || answered.chosenValue === null)) || (answered && answered.chosenValue === true)
-          const right = (option.right === !!checkedOption) || false
-          const wrong = (option.right !== !!checkedOption) || false
-          const border = right ? 'solid limegreen' : (wrong ? 'solid red' : null)
-          const opacity = (!wrong && !right) ? 0.5 : 1
+          const checkedOption =
+            (answered &&
+              (answered.chosenValue === undefined ||
+                answered.chosenValue === null)) ||
+            (answered && answered.chosenValue === true)
+          const right = option.right === !!checkedOption || false
+          const wrong = option.right !== !!checkedOption || false
+          const border = right ? 'solid limegreen' : wrong ? 'solid red' : null
+          const opacity = !wrong && !right ? 0.5 : 1
           const style = { border, opacity, borderRadius: '5px' }
           return (
             <Form.Field style={{ opacity: 0.9 }} key={option.text} disabled>
-              <label style={{ opacity: 0.9 }} >{option.text}</label>
-              <Form.Radio style={(option.right && style) || {}} checked={checkedOption === true} label="Kyllä" />
-              <Form.Radio style={(!option.right && style) || {}} checked={checkedOption !== true} label="Ei" />
+              <label style={{ opacity: 0.9 }}>{option.text}</label>
+              <Form.Radio
+                style={(option.right && style) || {}}
+                checked={checkedOption === true}
+                label="Oikein"
+              />
+              <Form.Radio
+                style={(!option.right && style) || {}}
+                checked={checkedOption !== true}
+                label="Väärin"
+              />
             </Form.Field>
           )
         })}
