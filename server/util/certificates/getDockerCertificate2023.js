ec5293dc3cfce8bae566ff4adbdac596ffaea5d6
Matti Luukkainen <mluukkai@iki.fi>
2023-03-23 19:11:54 +0200

    fix cert

--- server/util/certificates/getDockerCertificate2023.js
+++ server/util/certificates/getDockerCertificate2023.js
@@ -25,13 +25,15 @@ const getCertFile = require('./getCertFile')
 const getDockerCertificate = (url, name, submissions) => {
   const certSvg = dockerCert
   const htmlTemplate = dockerTemplate
-  const credits = submissionsToDocker2023Credits(submissions)
+  const credits = submissionsToDocker2023Credits(submissions).split('-')
+
+  const acualCredits = Number(credits[0]) + Number(credits[1]) + Number(credits[2])
 
   return getCertFile(htmlTemplate, {
     trueno: fontTrueno,
     certificate: certSvg,
     name,
-    text: `${credits}`,
+    text: `${acualCredits}`,
     url,
   })
 }
